7050e7578eba9037b47aff4f0426d10903f5dd9b
Natalie Ton-nu <natalietonnu@gmail.com>
2023-09-05 18:21:20 -0500

    Emulator Build Works

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World</Text>
+      <Text>Emulator Build Works</Text>
       <StatusBar style="auto" />
     </View>
   );
