ae8ab24a2e06afe2fa697643ee903a43e1368feb
xchar08 <jnp2934@mavs.uta.edu>
2023-10-26 02:11:42 -0500

    added code

--- dusk/App.js
+++ dusk/App.js
@@ -1,12 +1,39 @@
+//import { spawnSync } from 'child_process';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, Button, StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import cityscape from './assets/cityscape.jpg';
 
 export default function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const onPressLogIn = () => {
+    console.log('Learn More pressed');
+    setLoggedIn(true);
+  }
+  const onPressLogOut = () => {
+    console.log('Learn More pressed');
+    setLoggedIn(false);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>This is a test</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={cityscape} resizeMode="cover" style={styles.image}>
+        <Text style={styles.text}>Let's Go!</Text>
+          {isLoggedIn !== true && 
+            <Button onPress={onPressLogIn}
+              title="Log In"
+              color="#841584"
+              accessibilityLabel="Learn more about this purple button"
+            />
+          }
+          {isLoggedIn !== false && 
+            <Button onPress={onPressLogOut}
+              title="Log Out"
+              color="#841584"
+              accessibilityLabel="Learn more about this purple button"
+            />
+          }
+      </ImageBackground>
+      <StatusBar style="auto" />  
     </View>
   );
 }
@@ -17,5 +44,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    height: '100%',
+    width: '100%',
+    flex: 1,
+    justifyContent: 'center',
+    resizeMode: 'stretch',
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000c0',
   },
 });
